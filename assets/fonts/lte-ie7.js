259c9f127dd35380757c66b29041e3bead5634c6
Matt Boldt <me@mattboldt.com>
2014-07-16 21:06:06 -0500

    whoops

---
+++ assets/fonts/lte-ie7.js
@@ -0,0 +1,35 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-twitter' : '&#xe000;',
+			'icon-cog' : '&#xe001;',
+			'icon-user' : '&#xe002;',
+			'icon-github' : '&#xe003;',
+			'icon-forrst' : '&#xe004;',
+			'icon-tag' : '&#xe005;',
+			'icon-code' : '&#xe006;',
+			'icon-briefcase' : '&#xe007;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
