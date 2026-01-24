5ff75d2c0d4fad0d3edb5f49ca65013f0487c234
Matt Boldt <me@mattboldt.com>
2026-01-23 22:39:08 -0600

    updated GA tracking

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -71,23 +71,32 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'UA-11539016-1',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: false,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**'],
-        // Enables Google Optimize using your container Id
-        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'mattboldt.com',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-HC66W61KNV', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: 'OPT_CONTAINER_ID',
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: [],
+          // Defaults to https://www.googletagmanager.com
+          // origin: 'YOUR_SELF_HOSTED_ORIGIN',
+          // Delays processing pageview events on route update (in milliseconds)
+          // delayOnRouteUpdate: 0,
+        },
       },
     },
     `gatsby-plugin-sitemap`,
